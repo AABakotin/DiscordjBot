9c9d142595ec11f96bc04fae80b7940b8022f2a6
AABakotin <megabart57@gmail.com>
2025-02-11 13:37:53 +0300

    Добавлен выбор источника для поиска музыки

--- src/main/java/ru/discordj/bot/Main.java
+++ src/main/java/ru/discordj/bot/Main.java
@@ -4,6 +4,9 @@ import ru.discordj.bot.config.JdaConfig;
 
 public class Main {
     public static void main(String[] args) {
+        // Отключаем логи Apache HTTP Client
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+        
         JdaConfig.start(args);
     }
 }
--- src/main/java/ru/discordj/bot/events/ICommand.java
+++ src/main/java/ru/discordj/bot/events/ICommand.java
@@ -1,6 +1,7 @@
 package ru.discordj.bot.events;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface ICommand {
     List<OptionData> getOptions();
 
     void execute(SlashCommandInteractionEvent event);
+
+    default void onStringSelectInteraction(StringSelectInteractionEvent event) {}
 }
