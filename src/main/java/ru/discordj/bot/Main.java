7d7ed92d4a7d5a9aca85eb2aba1c6bf204ba4948
AABakotin <megabart57@gmail.com>
2025-04-08 22:45:13 +0300

    Настроено логирование с сохранением в файл и отключением предупреждений о куках

--- src/main/java/ru/discordj/bot/Main.java
+++ src/main/java/ru/discordj/bot/Main.java
@@ -1,9 +1,57 @@
 package ru.discordj.bot;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import ru.discordj.bot.config.JdaConfig;
 
+/**
+ * Главный класс приложения, инициализирующий всё необходимое для работы бота.
+ */
 public class Main {
-    public static void main(String[] args) {
+    
+    /**
+     * Метод запуска приложения. Инициализирует и запускает бота Discord.
+     * 
+     * @param args аргументы командной строки
+     * @throws Exception если произошла ошибка во время инициализации
+     */
+    public static void main(String[] args) throws Exception {
+        // Загружаем настройки логирования
+        configureLogging();
+        
+        // Запускаем бота с использованием существующей конфигурации
         JdaConfig.start(args);
+    }
+    
+    /**
+     * Настраивает логирование для перенаправления всех логов через SLF4J
+     */
+    private static void configureLogging() {
+        try {
+            // Загружаем конфигурацию из файла properties
+            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("logging.properties");
+            if (inputStream != null) {
+                try {
+                    LogManager.getLogManager().readConfiguration(inputStream);
+                } finally {
+                    inputStream.close();
+                }
+            } else {
+                System.err.println("Файл настроек логирования не найден");
+            }
+            
+            // Устанавливаем мост между JUL и SLF4J
+            // Удаляем существующие хендлеры у корневого логгера
+            SLF4JBridgeHandler.removeHandlersForRootLogger();
+            
+            // Устанавливаем мост между JUL и SLF4J
+            SLF4JBridgeHandler.install();
+            
+        } catch (IOException e) {
+            System.err.println("Ошибка при загрузке настроек логирования: " + e.getMessage());
+        }
     }
 }
