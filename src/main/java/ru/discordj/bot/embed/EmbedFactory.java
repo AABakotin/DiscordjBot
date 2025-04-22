b47de83bcf2e9a5670241e27f8f1f6e4c0b4d561
AABakotin <megabart57@gmail.com>
2025-04-22 18:19:33 +0300

    Упрощение класса EmbedFactory

--- src/main/java/ru/discordj/bot/embed/EmbedFactory.java
+++ src/main/java/ru/discordj/bot/embed/EmbedFactory.java
@@ -1,41 +1,15 @@
 package ru.discordj.bot.embed;
 
-public abstract class EmbedFactory {
-    private static final EmbedFactory instance = new DefaultEmbedFactory();
-    
-    public static EmbedFactory getInstance() {
-        return instance;
+public class EmbedFactory {
+    public static MusicEmbed createMusicEmbed() {
+        return new MusicEmbed();
     }
     
-    public abstract MusicEmbed createMusicEmbed();
-    public abstract WelcomeEmbed createWelcomeEmbed();
-    public abstract ConfigEmbed createConfigEmbed();
-    public abstract ServerStatusEmbed createServerStatusEmbed();
-}
-
-class DefaultEmbedFactory extends EmbedFactory {
-    private final MusicEmbed musicEmbed = new MusicEmbed();
-    private final WelcomeEmbed welcomeEmbed = new WelcomeEmbed();
-    private final ConfigEmbed configEmbed = new ConfigEmbed();
-    private final ServerStatusEmbed serverStatusEmbed = new ServerStatusEmbed();
-    
-    @Override
-    public MusicEmbed createMusicEmbed() {
-        return musicEmbed;
+    public static WelcomeEmbed createWelcomeEmbed() {
+        return new WelcomeEmbed();
     }
     
-    @Override
-    public WelcomeEmbed createWelcomeEmbed() {
-        return welcomeEmbed;
-    }
-    
-    @Override
-    public ConfigEmbed createConfigEmbed() {
-        return configEmbed;
-    }
-    
-    @Override
-    public ServerStatusEmbed createServerStatusEmbed() {
-        return serverStatusEmbed;
+    public static ServerStatusEmbed createServerStatusEmbed() {
+        return new ServerStatusEmbed();
     }
 } 
